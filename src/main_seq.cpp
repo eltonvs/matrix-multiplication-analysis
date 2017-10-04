9e01aec15647d1b4c335001a83179bc267712216
Elton Viana <eltonvs@outlook.com>
2017-10-03 21:55:33 -0300

    Add write file to main

--- src/main_seq.cpp
+++ src/main_seq.cpp
@@ -1,6 +1,15 @@
 #include <iostream>
+using std::cout;
+using std::endl;
 
-int main() {
-    std::cout << "Hello World Seq!\n";
+#include "write_file.h"
+
+int main(int argc, char *argv[]) {
+    if (argc == 2) {
+		WriteFile wf(argv[1], 0);
+		// wf.write_result_matrix(vector<vector<int>>());
+	} else {
+		cout << "Error" << endl;
+	}
     return 0;
 }
